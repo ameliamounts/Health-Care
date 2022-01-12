6a85925142669e263750550e90c6e9908c3768a9
ameliamounts <97560679+ameliamounts@users.noreply.github.com>
2022-01-12 10:18:52 -0500

    made the onboarding more readable
    still have bug onboarding the behaviors, prints enter symptom for -1

--- symtpomtracker.py
+++ symtpomtracker.py
@@ -1,5 +1,5 @@
 def main():
-    listOfDailyDataObjects = []
+    listOfDailyDataObjects = [] #this is what stores the DailyTrackerObjects objects, can access via indexing
 
     class DailyTracker():
         def __init__ (self, listSymptomTitles, listBehaviorTitles):
@@ -9,9 +9,6 @@ def main():
             self.dailyActivityTitles = listBehaviorTitles #list of the behaviors (but not the data)
             self.symptomData = []
             self.behaviorData = []
-        def addSymptomSeverity(listSymptomData, listBehaviorData):
-            symptomData = listSymptomData
-            behaviorData = listBehaviorData
 
         #ONBOARDING
     singleSymptom = 0
@@ -21,31 +18,41 @@ def main():
         print("These are the symptoms you have added: ")
         for j in listSymptomTitles:
             print(j)
-        singleSymptom = input("what symptoms do you want to track? (eg. headache, stomach pain, etc, to stop adding symptoms enter -1)? ")
-        listSymptomTitles.append(singleSymptom)
+        print()
+        print()
+        singleSymptom = input("what symptoms do you want to track? (eg. headache, to stop adding symptoms enter -1)? ")
+        if (singleSymptom!=-1):
+            listSymptomTitles.append(singleSymptom)
         i=i+1
+        
     i=0
     singleBehavior = 0
     listBehaviorTitles = []
+
     while (singleBehavior != "-1"):
+        print("These are the behaviors you have added: ")
         for j in listBehaviorTitles:
             print(j)
-        singleBehavior = input("what daily behaviors do you want to track (eg. hours of sleep, caloric intake, etc, to stop adding enter -1)? ")
-        listBehaviorTitles.append(singleBehavior)
+        print()
+        print()
+        singleBehavior = input("what daily behaviors do you want to track (eg. hours of sleep, miles walked etc, to stop adding enter -1)? ")
+        if (singleBehavior!=-1):
+            listBehaviorTitles.append(singleBehavior)
         i=i+1
 
 
-
+    print()
+    print()
     dailyData = DailyTracker(listSymptomTitles, listBehaviorTitles)
     p=0
     if (len(dailyData.symptomTitles)!=0):
         for p in range(0, (len(dailyData.symptomTitles)-1)):
-            symptomSeverity = input("On a scale of 0-5, with 5 being the worst, was your %s today?"% p)
-            dailyData.symptomData[p] = symptomSeverity
+            symptomSeverity = input("On a scale of 0-5, with 5 being the worst, what was your %s today? " %dailyData.symptomTitles[p])
+            dailyData.symptomData.append(symptomSeverity)
             p=p+1
     q=0
-    for q in dailyData.dailyActivityTitles[q]:
-        behaviorLevel = input("Add behavior... ")
+    for q in dailyData.dailyActivityTitles:
+        behaviorLevel = input("Describe your %s: " %q)
         dailyData.behaviorData.append(behaviorLevel)
     listOfDailyDataObjects.append(dailyData)
 
@@ -53,4 +60,8 @@ def main():
 main()
 
 #Still need to fix the UI, just trying to get the data structure to work
-
+#onboarding edits would include making it clearer as to what to add as the data, could make a range of what the values would be between
+#add a more unique prompt that uses the range of values
+#add in the graphing component
+#add section to save most recent graph to print again?
+#can do some wireframing/mockups to include in the presentation
